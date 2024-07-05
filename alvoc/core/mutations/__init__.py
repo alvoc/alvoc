e7f539130d6c7568f3a9ce864a7d4c4ce2277c60
mshunjan <manjothunjan@gmail.com>
2024-07-05 11:45:51 -0400

    refactor(mutations): update root level functions (wip)

--- alvoc/core/mutations/__init__.py
+++ alvoc/core/mutations/__init__.py
@@ -1,36 +1,53 @@
 from alvoc.core.mutations.prepare import prepare
 from alvoc.core.mutations.convert import aa, nt
-from alvoc.core.mutations.analyze import find_mutants as find_mutants_internal
 
 # Mutation root level functions
-def convert_aa(tax_id: str | None, genbank_file: str | None, mut: str, outdir: str):
+def convert_aa(tax_id: str | None, genbank_file: str | None, mut: str, outdir: str) -> list[str]:
     """
     Convert amino acid mutation to nucleotide mutations for a given virus
 
     Args:
-        tax_id (str, optional): Taxonomic ID of the virus. Required if 'genbank_file' is not provided.
-        genbank_file (str, optional): Path to the GenBank file. Required if 'tax_id' is not provided.
-        mut (str): The amino acid mutation in the format 'GENE:aaPOSITIONaaNEW'.
-        outdir (str, optional): Output directory for results and intermediate data. Defaults to the current directory.
+        tax_id : Taxonomic ID of the virus. Required if 'genbank_file' is not provided.
+        genbank_file : Path to the GenBank file. Required if 'tax_id' is not provided.
+        mut : The amino acid mutation in the format 'GENE:aaPOSITIONaaNEW'.
+        outdir : Output directory for results and intermediate data. Defaults to the current directory.
 
     Returns:
-        list: A list of nucleotide mutations.
+        A list of nucleotide mutations.
     """
     genes, seq = prepare(tax_id, genbank_file, outdir)
     return aa(mut, genes, seq)
 
-def convert_nt(tax_id: str | None, genbank_file: str | None, mut: str, outdir: str):
+def convert_nt(tax_id: str | None, genbank_file: str | None, mut: str, outdir: str) -> str:
     """
     Convert nucleotide mutation to amino acid mutation for a given virus.
 
     Args:
-        tax_id (str, optional): Taxonomic ID of the virus. Required if 'genbank_file' is not provided.
-        genbank_file (str, optional): Path to the GenBank file. Required if 'tax_id' is not provided.
-        mut (str): The nucleotide mutation in the format 'BASENPOSBASE'.
-        outdir (str, optional): Output directory for results and intermediate data. Defaults to the current directory.
+        tax_id : Taxonomic ID of the virus. Required if 'genbank_file' is not provided.
+        genbank_file : Path to the GenBank file. Required if 'tax_id' is not provided.
+        mut : The nucleotide mutation in the format 'BASENPOSBASE'.
+        outdir : Output directory for results and intermediate data. Defaults to the current directory.
 
     Returns:
-        str: The amino acid mutation.
+        The amino acid mutation.
     """
     genes, seq = prepare(tax_id, genbank_file, outdir)
     return nt(mut, genes, seq)
+
+def find_mutants(tax_id: str | None, genbank_file: str | None, file_path: str, mutations_path: str | None, min_depth: int, save_img: bool, csv: bool, outdir) -> None:
+    """Find mutations in sequencing data, either from BAM files or a sample list. Uses a dictionary of mutation lineages provided as a parameter.
+
+    Args:
+        tax_id : Taxonomic ID of the virus. Required if 'genbank_file' is not provided.
+        genbank_file : Path to the GenBank file. Required if 'tax_id' is not provided.
+        file_path: Path to the file containing sample information or BAM file.
+        mutations_path: Path to the file containing mutations or mutation identifier.
+        min_depth: Minimum depth for mutation analysis.
+        save_img: Whether to save a plot image.
+        csv: Whether to generate a CSV file.
+        outdir : Output directory for results and intermediate data. Defaults to the current directory.
+
+    Returns:
+        Prints number of reads with and without each mutation and generates a heatmap showing their frequencies.
+    """
+    genes, seq = prepare(tax_id, genbank_file, outdir)
--- alvoc/core/mutations/utils.py
+++ alvoc/core/mutations/utils.py
@@ -1,10 +1,11 @@
+from pysam import PileupColumn
 from alvoc.core.mutations.convert import aa
 
-def parse_snv(snv):
+def parse_snv(snv: str):
     """Parse a single nucleotide variant (SNV).
 
     Args:
-        snv (str): The SNV string in the format 'A123G'.
+        snv : The SNV string in the format 'A123G'.
 
     Returns:
         tuple: A tuple containing the original base, position, and new base.
@@ -15,11 +16,11 @@ def parse_snv(snv):
     return old_bp, pos, new_bp
 
 
-def snv_name(snv):
+def snv_name(snv: tuple):
     """Generate a name for the SNV based on its components.
 
     Args:
-        snv (tuple): A tuple containing the SNV components.
+        snv : A tuple containing the SNV components.
 
     Returns:
         str: A string representation of the SNV.
@@ -27,25 +28,27 @@ def snv_name(snv):
     return '{}{}{}'.format(*snv)
 
 
-def parse_mutation(mut):
+def parse_mutation(mut: str, genes: dict, seq: str):
     """Parse mutation to handle potential multiple SNVs.
 
     Args:
-        mut (str): Mutation string, which could include multiple SNVs.
+        mut : Mutation string, which could include multiple SNVs.
+        genes : Dictionary of genes with start and end positions.
+        seq : The nucleotide sequence.
 
     Returns:
         list: A list of parsed SNVs.
     """
-    muts = aa(mut) if ':' in mut else [mut]
+    muts = aa(mut, genes, seq) if ':' in mut else [mut]
     return [parse_snv(m) for m in muts]
 
 
-def mut_in_col(pileupcolumn, mut):
+def mut_in_col(pileupcolumn: PileupColumn, mut : str):
     """Count the occurrences and non-occurrences of a mutation in a pileup column.
 
     Args:
-        pileupcolumn (PileupColumn): A pileup column from a BAM file.
-        mut (str): The mutation to count.
+        pileupcolumn : A pileup column from a BAM file.
+        mut : The mutation to count.
 
     Returns:
         tuple: A tuple containing counts of mutations and non-mutations.
@@ -56,7 +59,9 @@ def mut_in_col(pileupcolumn, mut):
         if qpos is None:
             not_muts += 1
             continue
-        base = pileupread.alignment.query_sequence[qpos]
+        base = pileupread.alignment.query_sequence
+        if base is not None:
+            base = base[qpos]
         if base == mut:
             muts += 1
         else:
@@ -82,16 +87,18 @@ def print_mut_results(mut_results, min_depth):
     print('{}/{} mutations covered'.format(cov, len(mut_results)))
     print('{}/{} mutations detected'.format(mut_cov, len(mut_results)))
     
-def mut_idx(mut):
+def mut_idx(mut, genes, seq):
     """Get the index of the mutation for sorting purposes.
 
     Args:
         mut (str): Mutation string.
+        genes (dict): Dictionary of genes with start and end positions.
+        seq (str): The nucleotide sequence.
 
     Returns:
         int: The first genomic index of the mutation if available, otherwise -1.
     """
-    snvs = parse_mutation(mut)  # Assumes that parse_mutation doesn't require mut_lins
+    snvs = parse_mutation(mut, genes, seq)  # Assumes that parse_mutation doesn't require mut_lins
     return snvs[0][1] if snvs else -1
 
 def write_csv(sample_results, sample_names, min_depth, home_lab, mutants_name):
