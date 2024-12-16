248729c82b67a3eb37c3a863ae000c999a74b2ac
mshunjan <manjothunjan@gmail.com>
2024-12-16 02:40:25 -0500

    feat(cli): Add short flag for outdir flag

--- alvoc/cli/common.py
+++ alvoc/cli/common.py
@@ -10,7 +10,7 @@ virus: str = typer.Argument(
     ..., help="Taxonomic ID of the virus, or path to the GenBank file"
 )
 outdir: Path = typer.Option(
-    ".", help="Output directory for results and intermediate data"
+    ".", "--outdir", "-o", help="Output directory for results and intermediate data"
 )
 
 # Initialize Rich console
